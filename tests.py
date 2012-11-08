7f8e0c379210e1fceac2f5da3c7b4c8a4c2da0fb
jmartin <jmartin@zikzakmedia.com>
2012-11-08 12:02:11 +0100

    Initial release

---
+++ tests.py
@@ -0,0 +1,34 @@
+#This file is part of emailvalid. The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+'''
+Unit test for emailvalid
+'''
+
+import unittest
+import emailvalid
+
+EMAILS = (
+    ('username@example.com; anotheruser@example.com', True),
+    ('username@mi-domain', False),
+    ('another,user@example.com', False),
+    ('another;user@example.com', False),
+)
+
+class BankNumberTest(unittest.TestCase):
+    '''
+    Test Case for emailvalid
+    '''
+
+    def test_emails(self):
+        '''
+        Test eMails
+        '''
+        for email, result in EMAILS:
+            if result:
+                test = self.assert_
+            else:
+                test = self.assertFalse
+            test(emailvalid.check_email(email))
+
+if __name__ == '__main__':
+    unittest.main()
